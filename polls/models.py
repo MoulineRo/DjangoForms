b781307380e43d5765248d24a847a715f3a7d831
MoulineRo <simonovandsim@gmail.com>
2023-05-02 12:30:11 +0300

    second commit

--- polls/models.py
+++ polls/models.py
@@ -5,10 +5,7 @@ class TeachersM(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
-    # birthday = models.IntegerField(max_length=10)
-    birthday1 = models.IntegerField(default=4)
-    birthday2 = models.IntegerField(default=4)
-    birthday3 = models.IntegerField(default=4)
+    birthday = models.DateField()
     theme = models.CharField(max_length=100)
 
 
--- polls/views.py
+++ polls/views.py
@@ -9,17 +9,13 @@ def teacher(request):
     if request.method == "POST":
         form = Teachers(request.POST)
         if form.is_valid():
-            q1 = form.cleaned_data['name']
-            q2 = form.cleaned_data['surname']
-            q3 = form.cleaned_data['lastname']
-            q4 = form.cleaned_data['birthday1']
-            q5 = form.cleaned_data['birthday2']
-            q6 = form.cleaned_data['birthday3']
-            q7 = form.cleaned_data['theme']
-            TeachersM.objects.create(name=q1, surname=q2, lastname=q3, birthday1=q4, birthday2=q5, birthday3=q6,
-                                     theme=q7)
+            name = form.cleaned_data['name']
+            surname = form.cleaned_data['surname']
+            lastname = form.cleaned_data['lastname']
+            birthday = form.cleaned_data['birthday']
+            theme = form.cleaned_data['theme']
+            TeachersM.objects.create(name=name, surname=surname, lastname=lastname, birthday=birthday, theme=theme)
             return HttpResponseRedirect("/teachers/")
-    # else як у якості get-запроса
     else:
         form = Teachers()
     return render(request, "teacher.html", {"form": form})
@@ -28,21 +24,18 @@ def teacher(request):
 def teachers(request):
     if request.method == "GET":
         people = TeachersM.objects.all()
-        list = []
-        for person in people:
-            list.append(
-                f"{person.id} {person.name} {person.surname} {person.lastname} {person.birthday1}/{person.birthday2}/{person.birthday3} - {person.theme}")
-        return render(request, "teachers.html", {"people": list})
+        return render(request, "teachers.html", {"people": people})
 
+
+#
 
 def group(request):
     if request.method == "POST":
         form = Group(request.POST)
         if form.is_valid():
-            q = form.cleaned_data['name']
-            GroupM.objects.create(name=q)
+            name = form.cleaned_data['name']
+            GroupM.objects.create(name=name)
             return HttpResponseRedirect("/groups/")
-    # else як у якості get-запроса
     else:
         form = Group()
     return render(request, "group.html", {"form": form})
@@ -50,8 +43,5 @@ def group(request):
 
 def groups(request):
     if request.method == "GET":
-        people = GroupM.objects.all()
-        list = []
-        for person in people:
-            list.append(f"{person.id} {person.name}")
-        return render(request, "teachers.html", {"people": list})
+        groupe = GroupM.objects.all()
+        return render(request, "teachers.html", {"people": groupe})
